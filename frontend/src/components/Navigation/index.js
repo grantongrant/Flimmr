92b7fd08bb4c0f0b685c8d7115fd424da34288fb
Grant Russell <grant.ellis.russell@gmail.com>
2022-01-10 09:11:01 -0600

    added CSS styling to splash page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -22,12 +22,15 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Flimmr</NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+      <div className="navigation">
+        <ul>
+            <li>
+            <NavLink exact to="/">Flimmr</NavLink>
+            {isLoaded && sessionLinks}
+            </li>
+        </ul>
+      </div>
+
   );
 }
 
