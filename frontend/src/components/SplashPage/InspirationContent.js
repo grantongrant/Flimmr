755834aad7986cc696350f07b34388a8253fd047
Grant Russell <grant.ellis.russell@gmail.com>
2022-01-11 17:17:36 -0600

    added more css styling

--- frontend/src/components/SplashPage/InspirationContent.js
+++ frontend/src/components/SplashPage/InspirationContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './SplashPage.css';
+import '../../../src/index.css';
 
 const InspirationContent = () => {
 
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import InspirationContent from './InspirationContent';
-import './SplashPage.css';
+import '../../../src/index.css'
 
 const SplashPage = () => {
 
