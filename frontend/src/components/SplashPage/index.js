118b3a04a52cbb12ccb417d3719189e89a643795
Grant Russell <grant.ellis.russell@gmail.com>
2022-01-11 18:34:25 -0600

    continued to work on css landing page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -14,7 +14,20 @@ const SplashPage = () => {
                     </div>
                 </div>
                 <div className="splash-footer">
+                    <ul className="footer-technologies"></ul>
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>Javascript</li>
+                        <li>Node.js</li>
+                        <li>PostgresSQL</li>
+                        <li>HTML5</li>
+                        <li>CSS</li>
+                        <li>JSON API</li>
+                        <li>Git</li>
                 </div>
+                    {/* <ul className="grant-info">
+                        <li>GrantRussell+flimmr. Connecting people through photography.</li>
+                    </ul> */}
             </div>
         </div>
     )
